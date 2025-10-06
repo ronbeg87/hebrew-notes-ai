9681c2762ed6fa413c89620a7325afa05f813c38
RonBex <beg.ron@gmail.com>
2025-10-07 02:10:30 +0300

    remove old client dir + fix server deps

--- web-client/src/audioSource.ts
+++
@@ -1,6 +0,0 @@
-// Abstraction for audio sources
-export interface AudioSource {
-  start(): Promise<void>;
-  stop(): Promise<void>;
-  onData(callback: (audioBuffer: Float32Array) => void): void;
-}
--- web-client/src/transcriber.ts
+++
@@ -1,4 +0,0 @@
-// Abstraction for transcription engines
-export interface Transcriber {
-  transcribe(audioData: Blob | string): Promise<string>;
-}
